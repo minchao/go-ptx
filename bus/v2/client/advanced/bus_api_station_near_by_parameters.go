9caa0a02840bee6a1b5d1ea04207c1691d2d70b5
bot <73154172+code-automation@users.noreply.github.com>
2021-05-02 21:14:03 +0800

    chore(api): openapi specifications update (#63)
    Co-authored-by: minchao <minchao@users.noreply.github.com>

--- bus/v2/client/advanced/bus_api_station_near_by_parameters.go
+++ bus/v2/client/advanced/bus_api_station_near_by_parameters.go
@@ -92,7 +92,7 @@ type BusAPIStationNearByParams struct {
 
 	/* DollarSpatialFilter.
 
-	   空間過濾(最大搜尋半徑為1000公尺)
+	   空間過濾(最大搜尋半徑為1000公尺)，語法為nearby({Lat},{Lon},{DistanceInMeters})，例如nearby(25.047675, 121.517055, 100)
 	*/
 	DollarSpatialFilter string
 
